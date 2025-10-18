99f24806202b1ca48dcb53196892ffe5682b291e
codecason <kindom07@163.com>
2025-10-18 23:43:39 +0800

    add assignments

---
+++ clone.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/stanford-cs336/assignment1-basics.git
+git clone https://github.com/stanford-cs336/assignment2-systems.git
+git clone https://github.com/stanford-cs336/assignment3-scaling.git
+git clone https://github.com/stanford-cs336/assignment4-data.git
+git clone https://github.com/stanford-cs336/assignment5-alignment.git
+# Download log: 2025.10.18
